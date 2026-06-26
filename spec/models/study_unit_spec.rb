32d8089b17ca943a085abf97d859094b98251c09
Goto-star <goyu620@gmail.com>
2026-06-27 00:07:53 +0900

    feat: StudyUnit・Material・Userのモデルスペックの作成

--- spec/models/study_unit_spec.rb
+++ spec/models/study_unit_spec.rb
@@ -1,3 +1,66 @@
 require 'rails_helper'
 
-RSpec.describe StudyUnit, type: :model do ;end
+RSpec.describe StudyUnit, type: :model do
+  describe 'アソシエーション' do
+    it 'material に belongs_to で属すること' do
+      association = described_class.reflect_on_association(:material)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
+
+  describe 'バリデーション' do
+    context '必須属性がすべて揃っている場合' do
+      it '有効であること' do
+        expect(build(:study_unit)).to be_valid
+      end
+    end
+
+    context 'material が存在しない場合' do
+      it '無効であること' do
+        study_unit = build(:study_unit, material: nil)
+        expect(study_unit).to be_invalid
+        expect(study_unit.errors[:material]).to be_present
+      end
+    end
+
+    context 'title が空の場合' do
+      it '無効であること' do
+        study_unit = build(:study_unit, title: nil)
+        expect(study_unit).to be_invalid
+        expect(study_unit.errors[:title]).to be_present
+      end
+    end
+
+    context 'estimated_minutes が 0 の場合' do
+      it '無効であること' do
+        study_unit = build(:study_unit, estimated_minutes: 0)
+        expect(study_unit).to be_invalid
+        expect(study_unit.errors[:estimated_minutes]).to be_present
+      end
+    end
+
+    context 'estimated_minutes が空の場合' do
+      it '無効であること' do
+        study_unit = build(:study_unit, estimated_minutes: nil)
+        expect(study_unit).to be_invalid
+        expect(study_unit.errors[:estimated_minutes]).to be_present
+      end
+    end
+
+    context 'position が負の値の場合' do
+      it '無効であること' do
+        study_unit = build(:study_unit, position: -1)
+        expect(study_unit).to be_invalid
+        expect(study_unit.errors[:position]).to be_present
+      end
+    end
+
+    context 'position が整数でない場合' do
+      it '無効であること' do
+        study_unit = build(:study_unit, position: 1.5)
+        expect(study_unit).to be_invalid
+        expect(study_unit.errors[:position]).to be_present
+      end
+    end
+  end
+end
