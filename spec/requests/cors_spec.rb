9d2c1fb7f316a5542343e4fa9c010c3fe269df5a
Goto-star <goyu620@gmail.com>
2026-06-28 15:48:53 +0900

    test: CORSテスト名を日本語化

--- spec/requests/cors_spec.rb
+++ spec/requests/cors_spec.rb
@@ -1,16 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "CORS", type: :request do
-  context "when the request comes from the development frontend" do
-    it "allows the origin" do
+  context "開発用フロントエンドからリクエストされた場合" do
+    it "オリジンを許可する" do
       get "/up", headers: { "Origin" => "http://localhost:3000" }
 
       expect(response.headers["Access-Control-Allow-Origin"]).to eq("http://localhost:3000")
     end
   end
 
-  context "when the request comes from an unconfigured origin" do
-    it "does not allow the origin" do
+  context "設定されていないオリジンからリクエストされた場合" do
+    it "オリジンを許可しない" do
       get "/up", headers: { "Origin" => "https://example.com" }
 
       expect(response.headers).not_to include("Access-Control-Allow-Origin")
